0df765d09785fe103eedbfc60f21c5ef555c99fb
yukapooka <yuka.w.suzuki@gmail.com>
2026-03-19 01:52:22 +0000

    Deploying to gh-pages from @ yukapooka/portfolio@b89911ebf3ac477fac6fd0c16658c54415827cc9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/portfolio/case_studies/";
           },
-        },{id: "nav-cv",
-          title: "CV",
+        },{id: "nav-resume",
+          title: "resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
