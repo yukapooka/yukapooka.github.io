caecf0f6e30c06cf880db91c804bdf2b465b58a7
yukapooka <yuka.w.suzuki@gmail.com>
2026-03-20 12:20:01 +0000

    Deploying to gh-pages from @ yukapooka/yukapooka.github.io@97cb36b8fec71eb1313e40f8f60e5e6d17a7f60b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/resume/";
           },
-        },{id: "nav-resume",
-          title: "resume",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-case-studies",
           title: "case studies",
           description: "",
