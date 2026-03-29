50c211f38cb6dec65c38674157d422dde6ce6909
yukapooka <yuka.w.suzuki@gmail.com>
2026-03-29 11:22:34 +0000

    Deploying to gh-pages from @ yukapooka/yukapooka.github.io@30f2d267718e848b8b482e9d3907d73e1bc0c4c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,7 +407,7 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "case_studies-internal-analytics-platform",
           title: 'Internal analytics platform',
-          description: "From Fragmented Scripts to a Shared Platform - Building an Internal Analytics System for Sequencing R&amp;D",
+          description: "From Fragmented Scripts to a Shared Platform - Building an Internal Analytics System for R&amp;D",
           section: "Case_studies",handler: () => {
               window.location.href = "/case_studies/1_project/";
             },},{id: "case_studies-computational-capability-buildout",
